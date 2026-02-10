6f9446658a9d1e65b5ba01317d2de0fbad3e2729
Benemerito86 <benemerito.benjumea@gmail.com>
2026-02-10 19:59:57 +0100

    prueba css

--- gestionMedica/src/main/java/com/example/gestionmedica/controllers/DoctorController.java
+++ gestionMedica/src/main/java/com/example/gestionmedica/controllers/DoctorController.java
@@ -1,10 +1,1 @@
-package com.example.gestionmedica;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class DoctorController {
-    @FXML
-    private Label welcomeText;
-
-}
