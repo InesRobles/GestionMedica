e0c0571dcc7fcf2f347f26d66c91a518e490740e
inesr <inesrz333@gmail.com>
2026-02-10 17:28:01 +0100

    Actualización 1

---
+++ gestionMedica/src/main/java/com/example/gestionmedica/DoctorController.java
@@ -0,0 +1,10 @@
+package com.example.gestionmedica;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DoctorController {
+    @FXML
+    private Label welcomeText;
+
+}
