3be9d967c80afb25224c0f928318fafffc02d1f7
Inés <inesssrz33@gmail.com>
2026-02-10 16:10:52 +0100

    first commit

---
+++ gestionMedica/src/main/java/com/example/gestionmedica/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.gestionmedica;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
